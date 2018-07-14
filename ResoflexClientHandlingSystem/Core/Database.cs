774816c9404665705d3544a414cf055557af9dc4
TechNinjasv2 <40316378+TechNinjasv2@users.noreply.github.com>
2018-07-14 12:53:44 +0530

    Update Database.cs
    deleted comments from gimhana

--- ResoflexClientHandlingSystem/Core/Database.cs
+++ ResoflexClientHandlingSystem/Core/Database.cs
@@ -21,10 +21,6 @@ namespace ResoflexClientHandlingSystem.Core
             {
                 MessageBox.Show("Something went wrong!", "Add client", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-//Github is awesome
-//This works
-//I am feeling lucky
-//wass up
         }
     }
 }

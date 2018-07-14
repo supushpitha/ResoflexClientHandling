5090e51c59799c420d6dfc0636ec4e5833f73493
GimhanaDissanayake <38453002+GimhanaDissanayake@users.noreply.github.com>
2018-07-14 13:04:52 +0530

    Update Database.cs

--- ResoflexClientHandlingSystem/Core/Database.cs
+++ ResoflexClientHandlingSystem/Core/Database.cs
@@ -22,5 +22,6 @@ namespace ResoflexClientHandlingSystem.Core
                 MessageBox.Show("Something went wrong!", "Add client", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+        //sun is shining 
     }
 }

5f4ac01b85fc96dee96a818b7767eff88f8eceda
Geeth sandaru Madhushan <geeth.gsm@gmail.com>
2018-07-16 22:01:36 +0530

    Staff and User classes are redefined and Designation class added

--- ResoflexClientHandlingSystem/Role/Staff.cs
+++ ResoflexClientHandlingSystem/Role/Staff.cs
@@ -6,18 +6,45 @@ using System.Threading.Tasks;
 
 namespace ResoflexClientHandlingSystem.Role
 {
-    class Staff: User
+    class Staff
     {
         private int staffId;
+        private String firstName;
+        private String lastName;
+        private String nic;
+        private String address;
+        private String[] telNumber;
+        private String email;
+        private Designation designation;
+
         private List<Attendance> attendance;
 
         public Staff()
         {
         }
 
-        public Staff(int staffId,int userId, string userType, string firstName, string lastName, string NIC, string address, string[] telNumber, string email): base(userId, userType, firstName, lastName, NIC, address, telNumber, email)
+        public Staff(int staffId)
         {
             this.staffId = staffId;
-        } 
+        }
+
+        public Staff(string[] telNumber)
+        {
+            this.telNumber = telNumber;
+        }
+
+        public Staff(int staffId, Designation designation, string firstName, string lastName, string nic, string address, string[] telNumber, string email)
+        {
+            this.staffId = staffId;
+            this.designation = designation;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.nic = nic;
+            this.address = address;
+            this.telNumber = telNumber;
+            this.email = email;
+        }
+
+        internal List<Attendance> Attendance { get => attendance; set => attendance = value; }
     }
 }

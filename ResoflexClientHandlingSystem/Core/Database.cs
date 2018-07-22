3f382838464638056554327f955cbd4d49afdffe
DreadBot <DreadBot@DESKTOP-JU923TU>
2018-07-22 22:00:36 +0530

    update client function

--- ResoflexClientHandlingSystem/Core/Database.cs
+++ ResoflexClientHandlingSystem/Core/Database.cs
@@ -13,9 +13,29 @@ namespace ResoflexClientHandlingSystem.Core
     {
         public static void saveClient(Client client)
         {
+            string id = client.NIC;
+            string name = client.FirstName;
+            string[] tel = client.PhoneNo;
+            string add = client.Address;
             try
             {
-                DBConnection.updateDB("insert into client (name, tel, add) values ()");
+                DBConnection.updateDB("insert into client (name, tel, add) values ('"+name+"','"+tel+ "','"+add+"');");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Something went wrong!", "Add client", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        public static void updateClient(Client client) {
+
+            string nic = client.NIC;
+            string name = client.FirstName;
+            string[] tel = client.PhoneNo;
+            string add = client.Address;
+
+            try
+            {
+                DBConnection.updateDB("update client set name = '" + name + "', tel = '" + tel + "', add = '" + add + "' where nic = '"+nic+"');");
             }
             catch (Exception)
             {

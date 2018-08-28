52d2f11cb9aba06a2dda748fbe25099b2803887b
Geeth sandaru Madhushan <geeth.gsm@gmail.com>
2018-08-28 23:26:56 +0530

    Fixed Database class duplicated functions of Prabuddha after prabuddha's pull request merge

--- ResoflexClientHandlingSystem/Core/Database.cs
+++ ResoflexClientHandlingSystem/Core/Database.cs
@@ -138,6 +138,7 @@ namespace ResoflexClientHandlingSystem.Core
                 MessageBox.Show("User Permission can not be updated", "Update User", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+
         public static void deleteUser(User user)
         {
             try
@@ -152,59 +153,7 @@ namespace ResoflexClientHandlingSystem.Core
                 MessageBox.Show("", "Delete User", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-
-        //Adding a new schedule
-        public static Boolean addSchedule(Schedule schedule)
-        {
-            try
-            {
-                //for now
-                schedule.Vehicle = " ";
-                schedule.Mileage = 0;
-
-                DBConnection.updateDB("insert into schedule (proj_id, visit_type_id, vehicle_details, mileage, to_date_time, " +
-                    "from_date_time, to_do_list, resource, check_list, travelling_mode, accommodation, meals, logs) " +
-                    "VALUES (" + schedule.ProjectOfSchedule.ProjectID + ", " + schedule.Type.EventTypeId + ", '" + schedule.Vehicle + "'," +
-                    "" + schedule.Mileage + ", '" + schedule.To.ToString("yyyy-MM-dd HH:mm:ss") + "', '" + schedule.From.ToString("yyyy-MM-dd HH:mm:ss") + "', '" + schedule.TodoList + "', " +
-                    "'" + schedule.Resource + "', '" + schedule.Checklist + "', '" + schedule.TravelMode + "'," +
-                    " '" + schedule.AccommodationMode + "', '" + schedule.Meals + "', '" + schedule.Logs + "'); ");
-
-                foreach (var ary in schedule.ServEngineer)
-                {
-                    Staff s = (Staff) ary;
-
-                    DBConnection.updateDB("insert into schedule_technicians(sch_no, staff_id, proj_id) values (" + schedule.ScheduleId + ", " + s.StaffId + ", " + schedule.ProjectOfSchedule.ProjectID + ")");
-                }
-
-                return true;
-
-            }
-            catch (Exception e)
-            {
-
-                MessageBox.Show("Something went wrong!\n" + e.GetType() , "Add Schedule", MessageBoxButtons.OK, MessageBoxIcon.Error);
-
-                return false;
-            }
-        }
-
-        public static Boolean deleteSchedule(Schedule schedule)
-        {
-          
-            try
-            {
-                DBConnection.updateDB("delete from schedule where proj_id = " + schedule.ProjectOfSchedule.ProjectID + " and sch_no = " + schedule.ScheduleId + ";");
-
-                return true;
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show("Something went wrong!\n" + e.GetType(), "Schedule Deleted", MessageBoxButtons.OK, MessageBoxIcon.Error);
-
-                return false;
-            }
-        }
-      
+        
         public static void addProject(ResoflexClientHandlingSystem.Role.Project project)
         {
             Client client = project.ClientOfProject;

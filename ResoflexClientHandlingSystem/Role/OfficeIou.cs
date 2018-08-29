a9abc3920a41024d315cf5abd52f0f748e411dd6
Harindra <Harindra@DESKTOP-8N5PQPM>
2018-08-30 00:18:01 +0530

    Office_exp and salary forms

---
+++ ResoflexClientHandlingSystem/Role/OfficeIou.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResoflexClientHandlingSystem.Role
+{
+    class OfficeIou
+    {
+        private int iouNo;
+        private String name;
+        private DateTime date;
+        private String comment;
+        private double amount;
+
+
+        public int IouNo { get => iouNo; set => iouNo = value; }
+        public String Name { get => name; set => name = value; }
+        public DateTime Date { get => date; set => date = value; }
+        public String Comment { get => comment; set => comment = value; }
+        public double Amount { get => amount; set => amount = value; }
+
+        public OfficeIou(int iouNo, String name, DateTime date, String comment, double amount)
+        {
+
+            this.IouNo = iouNo;
+            this.Name = name;
+            this.Date = date;
+            this.Comment = comment;
+            this.Amount = amount;
+        }
+
+
+        public OfficeIou()
+        {
+        }
+    }
+}

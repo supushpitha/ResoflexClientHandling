63f7dc3e118ec749720a0717729037ad39c72f22
Geeth sandaru Madhushan <geeth.gsm@gmail.com>
2018-09-02 16:52:45 +0530

    ProjectForm dashboard

--- ResoflexClientHandlingSystem/ProjectExpenses/ProjectExpensesMainForm.cs
+++ ResoflexClientHandlingSystem/ProjectExpenses/ProjectExpensesMainForm.cs
@@ -2,6 +2,7 @@
 using ResoflexClientHandlingSystem.Core;
 using ResoflexClientHandlingSystem.ProjectExpenses.ProjectExpReports;
 using System;
+using System.Windows.Forms;
 
 namespace ResoflexClientHandlingSystem
 {
@@ -15,6 +16,110 @@ namespace ResoflexClientHandlingSystem
         private void ProjectExpensesMainForm_Load(object sender, EventArgs e)
         {
             monthCompProgressSpinner.Value = 75;
+            createChart();
+            spinProgress();
+        }
+
+        private void spinProgress()
+        {
+            double lastMonthExp = 0.0;
+            double thisMonthExp = 0.0;
+
+            MySqlDataReader reader = DBConnection.getData("select dateOfExp ym, IFNULL(SUM(amount), 0) as sum from exp_detail_event " +
+                "group by MONTH(dateOfExp) order by MONTH(dateOfExp)");
+
+            try
+            {
+                while (reader.Read())
+                {
+                    if ((reader.GetDateTime(0).Year == DateTime.Now.Year) && (reader.GetDateTime(0).Month == DateTime.Now.Month))
+                    {
+                        thisMonthExp = reader.GetDouble(1);
+                    }
+                    else if ((reader.GetDateTime(0).Year == DateTime.Now.Year) && (reader.GetDateTime(0).Month == DateTime.Now.Month - 1))
+                    {
+                        lastMonthExp = reader.GetDouble(1);
+                    }
+                }
+
+                reader.Close();
+
+                double progress = (thisMonthExp / lastMonthExp) * 100;
+
+                if (progress > 100)
+                {
+                    monthCompProgressSpinner.Style = MetroFramework.MetroColorStyle.Green;
+                    progress = progress % 100;
+                }
+                else if (progress < 100)
+                    monthCompProgressSpinner.Style = MetroFramework.MetroColorStyle.Red;
+                else
+                    monthCompProgressSpinner.Style = MetroFramework.MetroColorStyle.Yellow;
+
+                progLbl.Text = "" + (int) progress + "%";
+                monthCompProgressSpinner.Value = (int) progress;
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Error in creating chart!\n" + exc, "Chart drawer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                reader.Close();
+            }
+        }
+
+        private void createChart()
+        {
+            System.Windows.Forms.DataVisualization.Charting.Series series = chart1.Series[0];
+            
+            MySqlDataReader reader = DBConnection.getData("select MONTH(dateOfExp) month, IFNULL(SUM(amount), 0) as sum from exp_detail_event " +
+                "group by MONTH(dateOfExp)");
+
+            try
+            {
+                while (reader.Read())
+                {
+                    series.Points.AddXY(getMonthName(reader.GetInt16(0)), reader.GetDouble(1));
+                }
+
+                reader.Close();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Error in creating chart!\n" + exc, "Chart drawer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                reader.Close();
+            }
+        }
+
+        private string getMonthName(int monthNo)
+        {
+            switch (monthNo)
+            {
+                case 1:
+                    return "Jan";
+                case 2:
+                    return "Feb";
+                case 3:
+                    return "Mar";
+                case 4:
+                    return "Apr";
+                case 5:
+                    return "May";
+                case 6:
+                    return "Jun";
+                case 7:
+                    return "Jul";
+                case 8:
+                    return "Aug";
+                case 9:
+                    return "Sep";
+                case 10:
+                    return "Oct";
+                case 11:
+                    return "Nov";
+                case 12:
+                    return "Dec";
+                default:
+                    return "Non";
+            }
         }
 
         private void viewBalanceBtn_Click(object sender, EventArgs e)
@@ -64,25 +169,26 @@ namespace ResoflexClientHandlingSystem
             reader.Close();
 
             // Filling the Balance Tile
-            reader = DBConnection.getData("SELECT * FROM ((SELECT IFNULL(SUM(ev.amount), 0) as outExp FROM exp_detail_event ev) UNION (SELECT IFNULL(SUM(ip.amount), 0) inExp FROM project_exp_in_amount ip)) tmp");
+            reader = DBConnection.getData("SELECT * FROM ((SELECT IFNULL(SUM(ev.amount), 0) as outExp FROM exp_detail_event ev) " +
+                "UNION (SELECT IFNULL(SUM(ip.amount), 0) inExp FROM project_exp_in_amount ip)) tmp");
 
             if (reader.HasRows)
             {
                 double balance = 0.0;
-                double inE = 0.0, outEx = 0.0;
+                double inExp = 0.0, outExp = 0.0;
                 int count = 0;
 
                 while (reader.Read())
                 {
                     if (count == 0)
-                        outEx = reader.GetDouble(0);
+                        outExp = reader.GetDouble(0);
                     else if (count == 1)
-                        inE = reader.GetDouble(0);
+                        inExp = reader.GetDouble(0);
 
                     count++;
                 }
 
-                balance = inE - outEx;
+                balance = inExp - outExp;
 
                 balTile.Text = "Rs." + balance;
             }

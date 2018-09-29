2414602ac4f0f4f5d218a64a9bad0eb76b5e5783
Geeth sandaru Madhushan <geeth.gsm@gmail.com>
2018-09-30 03:24:43 +0530

    just

--- ResoflexClientHandlingSystem/Program.cs
+++ ResoflexClientHandlingSystem/Program.cs
@@ -19,7 +19,7 @@ namespace ResoflexClientHandlingSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ClientReportForm());
+            Application.Run(new Login());
         }
     }
 }
